76a4cba1f881b9c882d420d687d4aab423a4b183
Obertsean111 <Oberts1310@gmail.com>
2025-12-03 20:36:34 +0700

    adadad

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 import express from "express";
 import api from "./routes/api.js";
 import database from "./config/database.js";
+<<<<<<< Updated upstream
 import cors from "cors";
+=======
+import cors from "cors"
+
+>>>>>>> Stashed changes
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use(cors())
